4643d108e9a9d84e0f9eae116e1864d31787ed75
kskannandotwin <kskannan.win@gmail.com>
2023-02-03 06:13:00 +0530

    03.02.2023
    218. app logic using an illustrative flowchart has completed.

---
+++ stopwatch.js
@@ -0,0 +1,35 @@
+$(function() {
+    // variables
+        // app mode
+        // time counter
+        // variable for setInterval
+        // number of laps
+        // minutes, seconds, centiseconds for time and lap
+
+    // on app load show start and lap buttons
+
+    // click on start button
+
+        // mode on
+        // show stop and lap button
+        // start counter
+
+    // click on stop button
+        // show resume and reset buttons
+        // stop counter
+
+    // click on resume button
+        // show stop and lap buttons
+        // start action
+
+    // click on reset button
+        // reload the page
+
+    // click on lap button
+        // if mode is on
+            // stop action
+            // reset lap and print lap details
+            // start action
+
+    // functions
+});
