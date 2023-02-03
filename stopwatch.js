07bcb91d859132be366fd187ae0481c921291607
kskannandotwin <kskannan.win@gmail.com>
2023-02-03 19:22:05 +0530

    03.02.2023
    219. jquery code 1 has completed

--- stopwatch.js
+++ stopwatch.js
@@ -1,18 +1,27 @@
 $(function() {
     // variables
-        // app mode
-        // time counter
-        // variable for setInterval
-        // number of laps
+        var mode = 0; // app mode
+        var timeCounter = 0; // time counter
+        var lapCounter = 0; // lap counter
+        var action = 0; // variable for setInterval
+        var lapNumber = 0; // number of laps
         // minutes, seconds, centiseconds for time and lap
+        var timeMinutes, timeSeconds, timeCentiseconds, lapMinutes, lapSeconds, lapCentiseconds;
 
     // on app load show start and lap buttons
-
+    hideshowButtons('#startButton', '#lapButton');
+    
     // click on start button
-
+    $('#startButton').click(function() {
         // mode on
+        mode = 1;
+
         // show stop and lap button
+        hideshowButtons('#stopButton', '#lapButton');
+
         // start counter
+        startAction();
+    });        
 
     // click on stop button
         // show resume and reset buttons
@@ -32,4 +41,25 @@ $(function() {
             // start action
 
     // functions
+    // hideshowButtons function show two buttons
+    function hideshowButtons(x, y) {
+        $('.control').hide();
+        $(x).show();
+        $(y).show();
+    }
+
+    // start the counter
+    function startAction() {
+        action = setInterval(function(){
+            timeCounter++;
+            lapCounter++;
+            updateTime();
+       }, 10);
+    }
+
+    // update time: converts counters to min, sec, centisecond
+    function updateTime() {
+        // 1 minute = 60 * 100 centiseconds = 6000 centiseconds
+        timeMinutes = Math.floor(timeCounter / 6000);
+    }
 });
